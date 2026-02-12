0bc44cc6e206bb1b0dd9abaf5439487a5cb5eaf8
FerdaKoplo <sahmuraivan43@gmail.com>
2026-02-12 20:26:26 +0700

    components : added nav, sidebar, layout for the admin/pegawai panel

---
+++ resources/views/components/user/sidebar.blade.php
@@ -0,0 +1,36 @@
+<aside class="fixed left-0 top-24 w-80 h-[calc(100vh-6rem)] bg-neutral text-white transition-transform -translate-x-full lg:translate-x-0 overflow-y-auto">
+    
+    <div class="flex flex-col pt-12 w-full font-sans">
+
+        <a href="#" 
+           class="block w-full text-right py-4 px-8 font-semibold text-lg bg-secondary text-black mb-1">
+            Manajemen Pegawai
+        </a>
+
+        <a href="#" 
+           class="block w-full text-right py-4 px-8 font-semibold text-lg text-white hover:text-[#F4CE14] transition-colors duration-200 mb-1">
+            Manajemen Transaksi
+        </a>
+
+        <a href="#" 
+           class="block w-full text-right py-4 px-8 font-semibold text-lg text-white hover:text-[#F4CE14] transition-colors duration-200 mb-1">
+            Manajemen Katalog
+        </a>
+
+        <a href="#" 
+           class="block w-full text-right py-4 px-8 font-semibold text-lg text-white hover:text-[#F4CE14] transition-colors duration-200 mb-1">
+            Manajemen Produk Kustom
+        </a>
+
+        <a href="#" 
+           class="block w-full text-right py-4 px-8 font-semibold text-lg text-white hover:text-[#F4CE14] transition-colors duration-200 mb-1">
+            Statistik Transaksi
+        </a>
+
+        <a href="#" 
+           class="block w-full text-right py-4 px-8 font-semibold text-lg text-white hover:text-[#F4CE14] transition-colors duration-200 mb-1">
+            Traffic Website
+        </a>
+
+    </div>
+</aside>
