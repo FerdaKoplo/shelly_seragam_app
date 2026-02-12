1471cbf4cb21628370f6e7f3675c04f4788a22dd
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2026-02-12 22:25:27 +0700

    nvm

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id('user_id');
             $table->string('nama');
             $table->string('username')->unique();
