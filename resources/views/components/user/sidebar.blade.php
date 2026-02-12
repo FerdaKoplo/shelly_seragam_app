6e482aa0686655c904f264e415b509f91b6e38e7
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2026-02-12 23:18:57 +0700

    role based navigation

--- resources/views/components/user/sidebar.blade.php
+++ resources/views/components/user/sidebar.blade.php
@@ -1,41 +1,42 @@
 @php
 $active = 'bg-secondary text-black ';
 $inactive = 'text-white hover:text-black hover:bg-secondary hover:bg-opacity-70 transition-colors duration-200';
+$role = auth()->user()->role;
 @endphp
 
 <aside class="fixed left-0 top-24 w-80 h-[calc(100vh-6rem)] bg-neutral text-white transition-transform -translate-x-full lg:translate-x-0 overflow-y-auto">
-    
-    <div class="flex flex-col pt-12 w-full font-sans">
 
-        <a href="{{ route('manage.pegawai') }}" 
-           class="block w-full text-right py-4 px-8 font-semibold text-lg mb-1 {{ request()->routeIs('manage.pegawai') ? $active : $inactive }}">
+    <div class="flex flex-col pt-12 w-full font-sans">
+        @if($role === 'Admin' )
+        <a href="{{ route('manage.pegawai') }}"
+            class="block w-full text-right py-4 px-8 font-semibold text-lg mb-1 {{ request()->routeIs('manage.pegawai') ? $active : $inactive }}">
             Manajemen Pegawai
         </a>
-
-        <a href="{{ route('manage.transaksi') }}" 
-           class="block w-full text-right py-4 px-8 font-semibold text-lg mb-1 {{ request()->routeIs('manage.transaksi') ? $active : $inactive }}">
+        @endif
+        <a href="{{ route('manage.transaksi') }}"
+            class="block w-full text-right py-4 px-8 font-semibold text-lg mb-1 {{ request()->routeIs('manage.transaksi') ? $active : $inactive }}">
             Manajemen Transaksi
         </a>
 
-        <a href="{{ route('manage.katalog') }}" 
-           class="block w-full text-right py-4 px-8 font-semibold text-lg mb-1 {{ request()->routeIs('manage.katalog') ? $active : $inactive }}">
+        <a href="{{ route('manage.katalog') }}"
+            class="block w-full text-right py-4 px-8 font-semibold text-lg mb-1 {{ request()->routeIs('manage.katalog') ? $active : $inactive }}">
             Manajemen Katalog
         </a>
 
-        <a href="{{ route('manage.kustom') }}" 
-           class="block w-full text-right py-4 px-8 font-semibold text-lg mb-1 {{ request()->routeIs('manage.kustom') ? $active : $inactive }}">
+        <a href="{{ route('manage.kustom') }}"
+            class="block w-full text-right py-4 px-8 font-semibold text-lg mb-1 {{ request()->routeIs('manage.kustom') ? $active : $inactive }}">
             Manajemen Produk Kustom
         </a>
-
-        <a href="{{ route('statistik.transaksi') }}" 
-           class="block w-full text-right py-4 px-8 font-semibold text-lg transition-colors duration-200 mb-1 {{ request()->routeIs('statistik.transaksi') ? $active : $inactive }}">
+        @if($role === 'Admin' )
+        <a href="{{ route('statistik.transaksi') }}"
+            class="block w-full text-right py-4 px-8 font-semibold text-lg transition-colors duration-200 mb-1 {{ request()->routeIs('statistik.transaksi') ? $active : $inactive }}">
             Statistik Transaksi
         </a>
 
-        <a href="{{ route('traffic') }}" 
-           class="block w-full text-right py-4 px-8 font-semibold text-lg transition-colors duration-200 mb-1 {{ request()->routeIs('traffic') ? $active : $inactive }}">
+        <a href="{{ route('traffic') }}"
+            class="block w-full text-right py-4 px-8 font-semibold text-lg transition-colors duration-200 mb-1 {{ request()->routeIs('traffic') ? $active : $inactive }}">
             Traffic Website
         </a>
-
+        @endif
     </div>
 </aside>
