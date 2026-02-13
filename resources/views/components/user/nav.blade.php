75a1ceee549dd61789e2778e065ce0e407a993a2
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2026-02-13 21:25:51 +0700

    user nav, sorry puh

---
+++ resources/views/components/user/nav.blade.php
@@ -0,0 +1,9 @@
+<nav class="fixed top-0 z-50 w-full h-24 flex items-center justify-between px-14 bg-secondary">
+    <h1 class="font-bebas text-5xl">
+        SHELLY ADMIN PANEL
+    </h1>
+
+    <button class="font-bold text-warningPrimary">
+        <p>Logout</p>
+    </button>
+</nav>
