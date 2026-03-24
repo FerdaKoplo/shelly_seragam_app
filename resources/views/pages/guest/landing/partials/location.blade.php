29ae166b0e532ad17da96a60f43b7f0be4e2a783
FerdaKoplo <sahmuraivan43@gmail.com>
2026-03-24 12:12:42 +0700

    finishing manage kustom

--- resources/views/pages/guest/landing/partials/location.blade.php
+++ resources/views/pages/guest/landing/partials/location.blade.php
@@ -5,9 +5,11 @@
         <div class="relative p-2 border-2 border-[#F3C344] rounded-2xl w-full md:w-1/2">
             <div class="rounded-xl overflow-hidden h-[300px] bg-gray-100">
                 {{-- Replace with Actual Google Maps Embed --}}
-                <iframe class="w-full h-full border-0" allowfullscreen="" loading="lazy"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5040.910207810498!2d111.85567689999999!3d-8.0577917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e791de24e5325fd%3A0x5f44d96cba1eb7ec!2sToko%20Shelly%20Seragam!5e1!3m2!1sen!2sid!4v1770983615296!5m2!1sen!2sid"
-                    referrerpolicy="no-referrer-when-downgrade">
+                <iframe
+                    title="Shelly Seragam Location"
+                    class="w-full h-full border-0"
+                    allowfullscreen="" loading="lazy"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5040.910207810498!2d111.85567689999999!3d-8.0577917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e791de24e5325fd%3A0x5f44d96cba1eb7ec!2sToko%20Shelly%20Seragam!5e1!3m2!1sen!2sid!4v1770983615296!5m2!1sen!2sid"  referrerpolicy="no-referrer-when-downgrade">
                 </iframe>
             </div>
         </div>
