e3f5f30ee0423337186c768b07c14daaeaf931a7
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2026-03-30 20:03:10 +0700

    fix logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use function PHPUnit\Framework\isEmpty;
 
 class LoginController extends Controller
 {
